0db5413b9f29a22a9aecb583d3c2c831b631c755
Christian Stankowic <info@cstan.io>
2023-04-14 21:34:27 +0200

    add storage role

---
+++ roles/storage/molecule/default/tests/test_default.py
@@ -0,0 +1,27 @@
+"""
+Molecule unit tests
+"""
+import os
+import configparser
+import testinfra.utils.ansible_runner
+
+TESTINFRA_HOSTS = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']
+).get_hosts('all')
+
+
+def test_lvm(host):
+    """
+    test if storage was set-up correctly
+    """
+    # get variables from file
+    ansible_vars = host.ansible(
+        "include_vars",
+        "file=molecule/default/vars/main.yml"
+    )
+    # check file systems
+    for filesys in ansible_vars["ansible_facts"]["uyuni_filesystems"]:
+        assert host.mount_point(filesys["mountpoint"]).exists
+        assert host.mount_point(
+            filesys["mountpoint"]
+            ).filesystem == filesys["type"]
