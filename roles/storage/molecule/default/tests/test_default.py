c24cac664e5f16a129dcaa4fa2abaf9ae8fd56ca
Christian Stankowic <info@stankowic-development.net>
2023-07-14 20:56:54 +0200

    xfsprogs isn't installed in storage role if missing (#26)
    * [server] remove xfsprogs
    * [storage] add xfsprogs

--- roles/storage/molecule/default/tests/test_default.py
+++ roles/storage/molecule/default/tests/test_default.py
@@ -9,6 +9,21 @@ TESTINFRA_HOSTS = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts('all')
 
 
+def test_packages(host):
+    """
+    check if packages are installed
+    """
+    # get variables from file
+    ansible_vars = host.ansible(
+        "include_vars",
+        "file=molecule/default/vars/main.yml"
+    )
+    # check dependencies and Uyuni packages
+    for pkg in ansible_vars["ansible_facts"]["uyuni_storage_packages"] + \
+            ansible_vars["ansible_facts"]["uyuni_packages"]:
+        assert host.package(pkg).is_installed
+
+
 def test_lvm(host):
     """
     test if storage was set-up correctly
