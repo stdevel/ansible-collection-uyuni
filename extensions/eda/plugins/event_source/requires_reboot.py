8d04181da901111d0d58828cd906a644dfb3cd5c
Christian Stankowic <christian.stankowic@sva.de>
2023-09-28 17:03:00 +0200

    make actual API calls regarding reboots

--- extensions/eda/plugins/event_source/requires_reboot.py
+++ extensions/eda/plugins/event_source/requires_reboot.py
@@ -4,17 +4,32 @@ requires_reboot.py
 ansible-rulebook event source plugin that lists all hosts that require a reboot.
 
 Arguments:
+  - hostname: SUSE Manager/Uyuni hostname or IP address
+  - username: API username
+  - password: API password
   - delay: seconds to wait between events
+  - hosts: list of hosts to check for reboots
 
 Examples:
   sources:
     - stdevel.uyuni.requires_reboot:
+        hostname: uiuiuiuyuni.local.loc
+        username: admin
+        password: admin
         delay: 10
+        hosts:
+          - uyuni-client.pinkepank.loc
 
 """
 import asyncio
 from typing import Any, Dict
 import random
+from pyuyuni.management import JSONHTTPClient
+from pyuyuni.exceptions import (
+    InvalidCredentialsException
+)
+from pyuyuni.hosts import list_systems_requiring_reboot
+import logging
 
 
 async def main(queue: asyncio.Queue, args: Dict[str, Any]):
@@ -22,14 +37,35 @@ async def main(queue: asyncio.Queue, args: Dict[str, Any]):
     Main function that queries the Uyuni and returns whether hosts require reboots
     """
     delay = args.get("delay", 60)
+    hosts = args.get("hosts", [])
+    hostname = args.get("hostname")
+    username = args.get("username")
+    password = args.get("password")
+    port = args.get("port", 443)
+    verify = args.get("verify", False)
+
+    # access the Uyuni API
+    api_client = JSONHTTPClient(
+        logging.ERROR,
+        hostname,
+        username,
+        password,
+        port,
+        verify
+    )
+    _systems = list_systems_requiring_reboot(api_client)
 
     while True:
-        await queue.put(
-            {
-                "requires_reboot": bool(random.randrange(0, 2))
-            }
-        )
-        await asyncio.sleep(delay)
+        for host in hosts:
+            print(f"checking host {host}")
+            _flag = True if host in _systems else False
+            await queue.put(
+                {
+                    "host": host,
+                    "requires_reboot": _flag
+                }
+            )
+            await asyncio.sleep(delay)
 
 
 if __name__ == "__main__":
@@ -45,6 +81,5 @@ if __name__ == "__main__":
             """
             print(event)
 
-    # mock_arguments = dict()
     mock_arguments = {}
     asyncio.run(main(MockQueue(), mock_arguments))
